f3b997641159e3afaf79361573d58a55a948b1be
Christopher James <christopher.james.0125@gmail.com>
2016-11-22 13:20:22 -0600

    Update 00459 - Graph Connectivity.cpp

--- 00459 - Graph Connectivity.cpp
+++ 00459 - Graph Connectivity.cpp
@@ -6,16 +6,30 @@
 //                    Date: 10/27/2016
 //*****************************************************************
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
+	//read in the number of cases
 	int cases = 0;
 	cin >> cases;
-	
+
+	string line;
+	getline(cin, line);
+	getline(cin, line);
+	//for each case
 	for (int i = 0; i < cases; i++)
 	{
-		return 1;
+		getline(cin, line);
+		//get the all the lines.
+		while (line != "")
+		{
+			cout << line << endl;
+			getline(cin, line);
+		}
+		cout << endl;
+
 	}
 	return 0;
 }
