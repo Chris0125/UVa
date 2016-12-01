860ae1349d9ee25aa47a3a12e2aed4126d5a1b03
Christopher James <christopher.james.0125@gmail.com>
2016-12-01 13:23:21 -0600

    Update 10263 - Railway.cpp

--- 10263 - Railway.cpp
+++ 10263 - Railway.cpp
@@ -12,15 +12,18 @@ using namespace std;
 
 const double  EPS = 1e-9;
 
-
+//Point
 struct Point{
+	//A point has X-value and a Y-value
 	double x, y;
+	
+	//Default constructor
 	Point()
 	{
 		x = 0;
 		y = 0;
 	}
-
+	//Parameterized constructor
 	Point(double _x, double _y)
 	{
 		x = _x;
@@ -28,30 +31,36 @@ struct Point{
 	}
 };
 
+//A line has 3 coefficients
 struct Line{
 	double a, b, c;
 };
 
+//Convert two points to a line
 void Points_To_Line(Point p, Point q, Line & l)
 {
+	//find the gradient of the line going through
+	//these two points
 	double dy = p.y - q.y;
 	double dx = p.x - q.x;
 	
-
+	//if the gradient is not 0.
 	if (abs(dx) > EPS)
-	{
-		l.a = 
-		l.b = 
+	{	
+		//Equation of a line is this for (ax + by + c)
+		l.a = -(double) (p.y - q.y)/ (p.x - q.x);
+		l.b = 1.0;
 		l.c = p.y - ((dy / dx) * p.x);
 	}
 	else
 	{
+		//Equation of a line is this for (ax + by + c)
 		l.a = 1.0;
 		l.b = 0.0;
 		l.c = -1 * p.x;
 	}
 }
-
+//Vector
 struct vec{
 	double x, y;
 	vec(double _x, double _y)
@@ -114,41 +123,56 @@ double Dist_To_LineSegment(Point p, Point a, Point b, Point&c)
 
 int main()
 {
+	//Read in the values for point M
 	float Xm, Ym;
+	
+	//While we can keep reading in
 	while(cin >> Xm)
-  {
-  	cin>> Ym;
-  	int n, Px, Py;
-  	cin >> n;
-  	
-  	Point M(Xm,Ym);
-  	
-  	double mindist = 10000000000.0;
-  	double distance;
-  	
-  	Point A;
-  	Point B;
-  	Point C;
-  	Point S;
-  	cin >> A.x >> A.y;
-  	
-  	
-  	for (int i = 0; i < n; i++)
-  	{
-  	 
-  	  cin >> B.x >> B.y;
-  	  
-  	  distance = Dist_To_LineSegment(M,A,B,C);
-  	  
-  	  if (distance < mindist)
-  	  {
-  	    mindist = distance;
-  	    S = C;
-  	  }
-  	  A = B;
-  	}
-  	
-  	cout << fixed << setprecision(4) << S.x << endl << S.y << endl;
+	 {
+		//Read in Point-M's Y value;
+		cin>> Ym;
+		
+		//Read in N init the read in values
+		int n, Px, Py;
+		cin >> n;
+		
+		//Create point M
+		Point M(Xm,Ym);
+
+		//Set minimum distance to a big number
+		double mindist = 10000000000.0;
+		double distance;
+
+		//Init points
+		Point A;
+		Point B;
+		Point C;
+		Point S;
+		
+		//Read in the first point
+		cin >> A.x >> A.y;
+
+		//For every point we can get
+		for (int i = 0; i < n; i++)
+		{
+		  //Read in the second point
+		  cin >> B.x >> B.y;
+		  //Calculate the distane from M to the line segment AB
+		  distance = Dist_To_LineSegment(M,A,B,C);
+		  
+		  //If the distance is less than the minimal distance
+		  if (distance < mindist)
+		  {
+		    //Then this is the new minimum distance
+		    mindist = distance;
+		    //And this is the new closest point
+		    S = C;
+		  }
+		  //Set the second point to be the new first point
+		  A = B;
+		}
+		//Output the solution to the screen.
+		cout << fixed << setprecision(4) << S.x << endl << S.y << endl;
 	}
 	return 0;
 }
