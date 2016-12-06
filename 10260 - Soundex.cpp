8697645b49f8a684ff29269a218de37ffae63a75
Christopher James <christopher.james.0125@gmail.com>
2016-12-06 13:46:00 -0600

    Update 10260 - Soundex.cpp

--- 10260 - Soundex.cpp
+++ 10260 - Soundex.cpp
@@ -1,1 +1,29 @@
+//**************************************************************
+//               Extra Credit #3: Soundex
+//                        UVa# 10260
+//                  Name: Christopher James
+//               Advanced Programming Techniques
+//                    Date: 12/6/2016
+//*****************************************************************
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main()
+{
+  int Arr[128];
+  Arr['B'] = Arr['F'] = Arr['P'] = Arr['V'] = 1;
+  Arr['C'] = Arr['G'] = Arr['J'] = Arr['K'] = Arr['Q'] = Arr['S'] = Arr['X'] = Arr['Z'] = 2;
+  Arr['D'] = Arr['T'] = 3;
+  Arr['L'] = 4;
+  Arr['M'] = Arr['N'] = 5;
+  Arr['R'] = 6;
+  Arr['A'] = Arr['E'] = Arr['I'] = Arr['O'] = Arr['U'] = Arr['H'] = Arr['W'] = Arr['Y'] = 0;
+  
+	string input;
+  while(cin >> input)
+  {
+    
+  }
+		return 0;
+}
