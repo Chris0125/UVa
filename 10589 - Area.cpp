e0a41f0e1668f8e088359db97f4970b068769eb2
Christopher James <christopher.james.0125@gmail.com>
2016-12-08 10:31:53 -0600

    Create 10589 - Area.cpp

---
+++ 10589 - Area.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main() 
+{
+  int n, a, M = 0;
+  double area = 0;
+  cin >> n >> a;
+  
+  
+  for(int i =0; i<n; i++)
+  {
+    double coord1, coord2;
+    cin >> coord1 >> coord2;
+    
+    double dist1 = sqrt((coord1 * coord1) + (coord2 * coord2));
+    double dist2 = sqrt(((a - coord1)*(a - coord1)) + (coord2 * coord2));
+    double dist3 = sqrt((coord1 * coord1) + ((a - coord2)*(a - coord2)));
+    double dist4 = sqrt(((a - coord1)*(a - coord1)) + ((a - coord2)*(a - coord2)));
+    
+    if ((dist1 <= a) && (dist2 <= a) && (dist3 <= a) && (dist4 <= a))
+    {
+      M++;
+    }
+  }
+  area = M * a * a / n;
+  cout << setprecision(5) << fixed << area << endl;
+  return 0;
+}
