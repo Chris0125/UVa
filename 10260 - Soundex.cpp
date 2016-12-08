ad0e59a159de6df30f2f095c63edd7b4937018ba
Christopher James <christopher.james.0125@gmail.com>
2016-12-08 13:13:19 -0600

    Update 10260 - Soundex.cpp

--- 10260 - Soundex.cpp
+++ 10260 - Soundex.cpp
@@ -11,19 +11,46 @@ using namespace std;
 
 int main()
 {
-  int Arr[128];
-  Arr['B'] = Arr['F'] = Arr['P'] = Arr['V'] = 1;
-  Arr['C'] = Arr['G'] = Arr['J'] = Arr['K'] = Arr['Q'] = Arr['S'] = Arr['X'] = Arr['Z'] = 2;
-  Arr['D'] = Arr['T'] = 3;
-  Arr['L'] = 4;
-  Arr['M'] = Arr['N'] = 5;
-  Arr['R'] = 6;
-  Arr['A'] = Arr['E'] = Arr['I'] = Arr['O'] = Arr['U'] = Arr['H'] = Arr['W'] = Arr['Y'] = 0;
-  
+	//Set up translation array
+	int Arr[128];
+	Arr['B'] = Arr['F'] = Arr['P'] = Arr['V'] = 1;
+	Arr['C'] = Arr['G'] = Arr['J'] = Arr['K'] = Arr['Q'] = Arr['S'] = Arr['X'] = Arr['Z'] = 2;
+	Arr['D'] = Arr['T'] = 3;
+	Arr['L'] = 4;
+	Arr['M'] = Arr['N'] = 5;
+	Arr['R'] = 6;
+	Arr['A'] = Arr['E'] = Arr['I'] = Arr['O'] = Arr['U'] = Arr['H'] = Arr['W'] = Arr['Y'] = 0;
+
+	//While we can read in strings
 	string input;
-  while(cin >> input)
-  {
-    
-  }
-		return 0;
+	while (cin >> input)
+	{
+		//init output string
+		string output = "";
+		//init previous soundex code
+		int sp = 0;
+		//for every letter
+		for (int i = 0; i < input.length(); i++)
+		{
+			//get the soundex code
+			int s = Arr[input[i]];
+			//if the soundex code is not zero and not the same as before
+			if (s != 0 && s!=sp)
+			{
+				//add it to our new string
+				output.append(to_string(s));
+				//set the previous
+				sp = s;
+			}
+			//else if s == 0 and the previous is different
+			else if (s == 0 & s != sp)
+			{
+				//set the previous
+				sp = s;
+			}
+		}
+		//output string
+		cout << output << endl;
+	}
+	return 0;
 }
