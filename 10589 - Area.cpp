7472f59d99a9cbcb0cb6d3d42d04ce275f3723bf
Christopher James <christopher.james.0125@gmail.com>
2016-12-08 11:00:55 -0600

    Update 10589 - Area.cpp

--- 10589 - Area.cpp
+++ 10589 - Area.cpp
@@ -4,29 +4,31 @@
 
 using namespace std;
 
-int main() 
+int main()
 {
-  int n, a, M = 0;
-  double area = 0;
-  cin >> n >> a;
-  
-  
-  for(int i =0; i<n; i++)
-  {
-    double coord1, coord2;
-    cin >> coord1 >> coord2;
-    
-    double dist1 = sqrt((coord1 * coord1) + (coord2 * coord2));
-    double dist2 = sqrt(((a - coord1)*(a - coord1)) + (coord2 * coord2));
-    double dist3 = sqrt((coord1 * coord1) + ((a - coord2)*(a - coord2)));
-    double dist4 = sqrt(((a - coord1)*(a - coord1)) + ((a - coord2)*(a - coord2)));
-    
-    if ((dist1 <= a) && (dist2 <= a) && (dist3 <= a) && (dist4 <= a))
-    {
-      M++;
-    }
-  }
-  area = M * a * a / n;
-  cout << setprecision(5) << fixed << area << endl;
-  return 0;
+	int n, a, M = 0;
+	double area = 0;
+	while (cin >> n >> a)
+	{
+		if (n == 0)
+			break;
+		for (int i = 0; i < n; i++)
+		{
+			double coord1, coord2;
+			cin >> coord1 >> coord2;
+
+			double dist1 = sqrt((coord1 * coord1) + (coord2 * coord2));
+			double dist2 = sqrt(((a - coord1)*(a - coord1)) + (coord2 * coord2));
+			double dist3 = sqrt((coord1 * coord1) + ((a - coord2)*(a - coord2)));
+			double dist4 = sqrt(((a - coord1)*(a - coord1)) + ((a - coord2)*(a - coord2)));
+
+			if ((dist1 <= a) && (dist2 <= a) && (dist3 <= a) && (dist4 <= a))
+			{
+				M++;
+			}
+		}
+		area = (double)M * (double)a * (double)a / (double)n;
+		cout << fixed << setprecision(5) << area << endl;
+	}
+	return 0;
 }
